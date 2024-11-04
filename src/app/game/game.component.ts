518907cd564ae629d305192b1fa6a0039a13dda8
PatrickKanert <patrickkanert@gmail.com>
2024-11-04 20:02:45 +0100

    upadate

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,19 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
-
   pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
 
-  takeCard() {
-    this.pickCardAnimation = true;
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
   }
 
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) this.currentCard = this.game.stack.pop() || '';
+    this.pickCardAnimation = true;
+
+    setTimeout(() => {
+      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = false;
+    }, 1000);
+  }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,36 @@
+export class Game {
+  public players: string[] = ['Patrick', 'Lisa', 'Jakob', 'Anna'];
+  public stack: string[] = [];
+  public playedCards: string[] = [];
+  public currentPlayer: number = 0;
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('ace_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('diamonds_' + i);
+      this.stack.push('hearts_' + i);
+    }
+    shuffle(this.stack);
+  }
+}
+
+function shuffle(array: string[]): string[] {
+  var currentIndex = array.length,
+    temoraryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temoraryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temoraryValue;
+  }
+
+  return array;
+}
